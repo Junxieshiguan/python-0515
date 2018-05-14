5d15a28ff532179b442af47fee88d5d53f565275
junxieshiguan <854153547@qq.com>
2018-05-14 20:38:20 +0800

    修改后的第2次

--- newtex.py
+++ newtex.py
@@ -2,3 +2,19 @@ a = 10
 b = 20
 c = a+45+pow(b,3)
 print('I am a good man, my charm is:%d'%c)
+
+Git is a distributed version control system.
+Git is free software.
+Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.Git is a distributed version control system.
+Git is free software.
